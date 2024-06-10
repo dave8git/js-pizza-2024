687a6b4b728f34a699aaf3fb424cb1f177eaf1df
poldave111 <poldave111@gmail.com>
2024-06-10 19:12:42 +0200

    Add getters and setters

--- dist/js/components/AmountWidget.js
+++ dist/js/components/AmountWidget.js
@@ -1,65 +1,77 @@
-import {settings, select} from '../settings.js';
+import { settings, select } from "./../settings.js";
+import BaseWidget from "./BaseWidget.js";
 
-class AmountWidget{
+class AmountWidget extends BaseWidget{
     constructor(element) {
-      const thisWidget = this; 
-      thisWidget.getElements(element);
-      //thisWidget.setValue(thisWidget.input.value);
-      if(thisWidget.input.value) {  // jeżeli coś jest w inpucie
-        thisWidget.setValue(thisWidget.input.value); // to podaj do setValue (i użyj) tego co jest wpisane
-      } else {
-        thisWidget.setValue(settings.amountWidget.defaultValue); // jeżeli nie ma nic, to ustaw defaultValue
-      }
-      thisWidget.initActions();
-    }
-
-    getElements(element) { 
-      const thisWidget = this; 
-      thisWidget.element = element; 
-      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease); 
-    }
-
-    setValue(value) {
+      super(element, settings.amountWidget.defaultValue);
       const thisWidget = this;
-      const newValue = parseInt(value);
+      // console.log('AmountWidget:', thisWidget);
+      // console.log('constructor arguments:', element);
+      thisWidget.getElements(element);
+      //thisWidget.setValue(thisWidget.dom.input.value=settings.amountWidget.defaultValue);
+      
+      thisWidget.initActions();
+      //thisWidget.setValue();
+      thisWidget.renderValue();
+     
+    }
 
-      if(thisWidget.value !== newValue && !isNaN(newValue)) {
-        if(newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
-          thisWidget.value = newValue;
-          thisWidget.announce();
-        }
-      }
-      /* TODO: Add validation */ 
-      thisWidget.input.value = thisWidget.value;
+    isValid(value) {
+      return !isNaN(value) 
+          // && value <= settings.amountWidget.defaultMax 
+          // && value >= settings.amountWidget.defaultMin;
+    }
+
+    renderValue() {
+      const thisWidget = this;
+
+      thisWidget.dom.input.value = thisWidget.value;
+    }
+
+    getElements() {
+      const thisWidget = this;
+
+      //thisWidget.dom.wrapper = element;
+      console.log('thisWidget.dom.wrapper.querySelector(select.widgets.amount.input)', thisWidget.dom.wrapper.querySelector(select.widgets.amount.input))
+      thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+      thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
     }
 
     initActions() {
       const thisWidget = this;
-      thisWidget.input.addEventListener('change', function() {
-        thisWidget.setValue(thisWidget.input.value);
-      })
-      thisWidget.linkDecrease.addEventListener('click', function() {
-      if(thisWidget.value >= 0) {
-        thisWidget.setValue(thisWidget.value - 1);
-      }
+      //console.log(thisWidget.dom.input.value);
+      thisWidget.dom.input.addEventListener('change', function (event) {
+        event.preventDefault();
+        //thisWidget.setValue(thisWidget.dom.input.value);
+        thisWidget.value = thisWidget.dom.input.value;
       });
-      thisWidget.linkIncrease.addEventListener('click', function() {
-      if(thisWidget.value <= 9) {
-        thisWidget.setValue(thisWidget.value + 1);
-      }
+      
+
+      thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        if(thisWidget.value >= 0) {
+          thisWidget.setValue(thisWidget.value-1);
+        }
+      });
+
+      thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        if(thisWidget.value <= 9) {
+          thisWidget.setValue(thisWidget.value+1);
+        } 
         
-      })
+      });
     }
-    announce(){
-      const thisWidget = this;
-      // const event = new Event('updated');
+
+    announce() {
+      const thisWidget = this; 
       const event = new CustomEvent('updated', {
         bubbles: true
       });
-      console.log('stworzył się customEvent updated i powinno działać')
-      thisWidget.element.dispatchEvent(event);
+      thisWidget.dom.wrapper.dispatchEvent(event);
+      //console.log(event);
     }
   }
 
---
+++ src/js/components/BaseWidget.js
@@ -0,0 +1,68 @@
+import {settings, select} from '../settings.js';
+class BaseWidget {
+    constructor(wrapperElement, initialValue) {
+        const thisWidget = this; 
+
+        thisWidget.dom = {};
+        thisWidget.dom.wrapper = wrapperElement; 
+     
+
+        thisWidget.correctValue = initialValue;
+    }
+
+    get value() {
+        const thisWidget = this; 
+
+        return thisWidget.correctValue; 
+    }
+
+    set value(value) {
+        const thisWidget = this;
+  
+        const newValue = thisWidget.parseValue(value); // input nawet o typie 'number' zawsze zwraca wartość w formacie tekstowym. 
+        // jeżeli parseInt nie będzie w stanie skonwertować tego co otrzyma zwróci null
+       
+        if(thisWidget.correctValue !== newValue) {
+          thisWidget.correctValue = newValue;
+            thisWidget.announce();
+            //console.log('newValue', newValue);
+        } 
+       // thisWidget.dom.input.value = thisWidget.correctValue;
+       thisWidget.renderValue();
+      }
+
+    setValue(value) {
+        const thisWidget = this; 
+
+        thisWidget.value = value;
+    }
+    //   setValue(value) {
+    //     const thisWidget = this; 
+
+    //     thisWidget.correctValue = value;
+    //   }
+      parseValue(value) {
+        return parseInt(value);
+      }
+  
+      isValid(value) {
+        return !isNaN(value);
+      }
+  
+      renderValue() {
+        const thisWidget = this;
+  
+        thisWidget.dom.wrapper.innerHTML = thisWidget.value;
+      }
+
+      announce() {
+        const thisWidget = this; 
+        const event = new CustomEvent('updated', {
+          bubbles: true
+        });
+        thisWidget.dom.wrapper.dispatchEvent(event);
+        //console.log(event);
+      }
+}
+
+export default BaseWidget;
